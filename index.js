f12ebb384a9914857e37d3a029ccb3157658c453
balasakthi <balasakthi.g@gmail.com>
2025-04-04 11:07:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/api/course", (req, res) => {
+  res.send([1, 2, 4]);
+});
+
+app.listen(3000, () => console.log("listening on port 3000..."));
